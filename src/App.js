2b9ca7cba96d03b496f3087012cc65f4ffe0f1c0
Dickson Palomeras <dicksonpal_021@yahoo.com.ph>
2022-03-27 20:05:46 +0800

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import firebase from 'firebase/compat/app';
+import { initApp, db, uiConfig } from './firebase';
+import 'firebase/compat/auth';
+
+// Components
+import SignIn from './components/SignIn';
+import Home from './components/Home';
+
+// Styles
+import './styles.scss';
+
+// JSON
+import candidates from './candidates.json';
 
 function App() {
+  initApp();
+  const [user, setUser] = useState(null);
+
+  const signinProps = {
+    firebase,
+    uiConfig,
+    setUser,
+  };
+
+  const homeProps = {
+    db,
+    firebase,
+    candidates,
+    user,
+    setUser,
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <SignIn {...signinProps} />
+          </Route>
+          <Route exact path="/home">
+            <Home {...homeProps} />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyA9TRcmGNCC8zF1YEZB67chxeBwK5Xp4HU',
+  authDomain: 'react-firebase-auth-93c06.firebaseapp.com',
+  projectId: 'react-firebase-auth-93c06',
+  storageBucket: 'react-firebase-auth-93c06.appspot.com',
+  messagingSenderId: '388513152774',
+  appId: '1:388513152774:web:8513c438ce7c0402fee887',
+};
+
+export default firebaseConfig;
